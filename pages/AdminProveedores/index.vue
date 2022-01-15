d73a34230e425f93128ce8caa3c3f369574e05c5
diego alejandro henao <dahdhenao@gmail.com>
2022-01-15 15:51:39 -0500

    funcion de cargar coordenadas inicial para la empresa proveedora

--- pages/AdminProveedores/index.vue
+++ pages/AdminProveedores/index.vue
@@ -16,9 +16,40 @@
             </b-navbar-nav>
         </b-collapse>
     </b-navbar>
+    {{$auth.$state.user}}   
     <div ref="map" id="map" :style="mapStile">
     </div>
-    <input type="text" class="input-form" v-model="valorBuscar" @keyup="viendoDireccion()">
+    <div class="alert alert-danger input-group" v-if="!$auth.user.coordenadas && !viendoMapa">
+      <h4><strong>Aun no has agregado las cordenadas. Actualiza la 
+      informacion para poder hacer uso de las funcionalidades de la app &ensp; </strong></h4><div class="btn btn-primary" @click="validarCoordenadas()">Agregar coordenadas</div>
+    </div>
+    <div v-if="viendoMapa" class="p-3">
+      <div class="alert alert-warning input-group" v-if="ayudas.segunda && valorBuscar.length>9">
+        <div >
+          <strong>Revisa el mapa, si el marcador </strong> <b-icon-geo-alt-fill variant="danger" font-scale="2"></b-icon-geo-alt-fill> no a sido posicionado en la direccion esperada, 
+          escribe una direccion cercana, e intenta ubicar el marcador haciendo click en el mapa.
+        </div>
+      </div>
+      <div v-if="ayudas.primera" class="row alert alert-info p-3">
+        <div class="col md 8 text-center">
+          Ingresa el nombre de la empresa o la direccion, seguido por el nombre de la ciudad. <br>
+          <strong>Ejemplo ingresando el nombre:</strong> Temlo Dosquebradas Risaralda, <br>
+          <strong>Ejemplo ingresando la direccion:</strong> Calle 9 # 1-74 Dosquebradas Risaralda. <br>
+          <strong>
+            El mapa te ira posicionando de acuerdo a la informacion. <br>
+            Recuerda que puedes dar click en la ubicacion correcta para posicionar el marcador y establecer las coordenadas.
+          </strong>
+        </div>
+        <div class="col-md-4 text-center p-2">
+          <b-icon icon="arrow-down" animation="cylon-vertical" font-scale="3"></b-icon>
+        </div>
+      </div>
+      <input type="text" class="form-control border border-primary" v-model="valorBuscar" @keyup="viendoDireccion()" v-if="!$auth.user.coordenadas"/>
+      <div class="alert alert-warning mt-3" v-if="ayudas.segunda && valorBuscar.length>9">
+         si el marcador <b-icon-geo-alt-fill variant="danger" font-scale="2"></b-icon-geo-alt-fill> esta en la ubicacion exacta, pesiona el boton Guardar para agregar estas coordenadas a la informacion de la empresa: <strong> {{$auth.user.empresa}}</strong>
+        <div class="btn btn-primary" @click="guardarCoordenadas()">Guardar {{coordenadas}}</div>
+      </div>
+    </div>
     <div class="btn btn-success" @click="cerrarMapa()" v-if="viendoMapa">cerrar</div>
     <div v-if="moduloEmpleados">
         <barra-admin-proveedores/>
@@ -26,7 +57,6 @@
     <div v-if="moduloCatalogo">
         <catalogo-proveedores/>
     </div>
-    {{$auth.$state.user}}
   </div>
 </template>
 
@@ -47,16 +77,24 @@ export default {
       },
       valorBuscar:'',
       loader:null,
+      ayudas:{
+        primera:false,
+        segunda:false,
+        tercera:false,
+      },
+      coordenadas:{
+        lat:0,
+        lng:0,
+      }
     }
   },
     async mounted(){
-      this.validarCoordenadas()
-      this.validandoLoguin()
       this.loader = new Loader({
           apiKey: 'AIzaSyBUug4MX79d36HIpp0O0lnESVzj4xI9LYM',
           version: "weekly",
-          libraries: ["geometry"],
+          libraries: ["geometry","places"],
       });
+      this.validandoLoguin()
     },
   methods: {
     validandoLoguin(){
@@ -86,7 +124,8 @@ export default {
             this.cargarTemporizador(this.$auth.$storage.getUniversal('horaSalidaAdminProveedor'))
         }else{
             console.log('no exsite la hora de salida')
-        }        
+        } 
+        this.validarCoordenadas()       
       } 
     },
     cargarTemporizador(data){
@@ -130,6 +169,7 @@ export default {
           /* Read more about isConfirmed, isDenied below */
           if (result.isConfirmed) {   
               const newToken = await this.$axios.$post('/newLoginProveedor/actualizarToken')
+              console.log(newToken)
               if(newToken.success){
                   this.$auth.strategy.token.set(newToken.token)
                   this.$auth.strategy.token.sync()
@@ -151,7 +191,7 @@ export default {
       if(!this.$auth.user.coordenadas){
         this.$swal.fire({
           title:'Porfavor',
-          text:'Ingresa las coordenadas de la empresa',
+          text:'Ingresa las coordenadas de la empresa, el sistema te guiara como hacerlo',
           icon: 'warning',
           showDenyButton: true,
           confirmButtonText: 'Cargar coordenadas',
@@ -159,19 +199,34 @@ export default {
         }).then(async (result) => {
           /* Read more about isConfirmed, isDenied below */
           if (result.isConfirmed) {  
-            let wifiAccessPoints = {
-              macAddress: "9c:1c:12:b0:45:f1",
-              age:0
-            }
-            /* const coordenadas  = await this.$axios.$post('https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyBUug4MX79d36HIpp0O0lnESVzj4xI9LYM',wifiAccessPoints)    
-            console.log(coordenadas)   */  
-            //this.cargarCoordenadass(coordenadas) 
-            navigator.geolocation.watchPosition(this.cargarCoordenadass)          
+            this.cargarMapaInicial()       
           } else if (result.isDenied) {
             this.$swal.fire('Changes are not saved', '', 'info')
           }
         })
       }
+    },
+    cargarMapaInicial(){     
+      this.loader.load().then(() => {
+        let lat = 4.7492485
+        let lng = -75.9129166
+        let latLng = new google.maps.LatLng(lat, lng);
+        this.map = new google.maps.Map(this.$refs.map, {
+          center:latLng,
+          zoom: 17,
+          mapTypeId:'roadmap',
+        });
+        new google.maps.Marker({
+          position: latLng,
+          map: this.map,
+        });
+      });
+      this.mapStile={
+        width: '100%',
+        height: '350px'
+      }
+      this.viendoMapa = true
+      this.ayudas.primera = true
     },
     cerrarMapa(){
       this.map = null
@@ -195,48 +250,82 @@ export default {
           position: latLng,
           map: this.map,
         });
-        
+        /* const card = document.getElementById("pac-card");
+        this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(card); */
       });
       this.mapStile={
         width: '100%',
         height: '350px'
-      },
+      }
       this.viendoMapa = true
     },
     async viendoDireccion(){
-      if(this.valorBuscar.length>3){
-          const geoCoder = new google.maps.Geocoder()
-          geoCoder.geocode({
-            'address':this.valorBuscar},function (resultado, status){
-            if (status === 'OK') {
-              var resultados = resultado[0].geometry.location
-              let lat = resultados.lat()
-              let lng = resultados.lng()
-              let latLng = new google.maps.LatLng(lat, lng);
-              let map = new google.maps.Map(document.getElementById('map'), {
-                center:latLng,
-                zoom: 17,
-                mapTypeId:'roadmap',
-              });
-              map.setCenter(resultado[0].geometry.location)
-              new google.maps.Marker({                
-                position: latLng, 
-                map,
-              });
+      if(this.valorBuscar.length>5){
+        this.ayudas.segunda = true      
+        const geoCoder = new google.maps.Geocoder()
+        geoCoder.geocode({
+          'address':this.valorBuscar,
+          componentRestrictions: {
+            country: "CO",
+            //postalCode: "6600",
+          }
+        },(resultado, status)=>{
+          if (status === 'OK') {
+            var resultados = resultado[0].geometry.location
+            let lat = resultados.lat()
+            let lng = resultados.lng()
+            this.coordenadas = {
+              lat : resultados.lat(),
+              lng : resultados.lng()
             }
-          });
-        }
-    },
-    async cargarCoordenadass(position){
-      let coordenadas = {
-        latitude:0,
-        longitude:0,
-        _id:this.$auth.user.idEmpresa
+            let latLng = new google.maps.LatLng(lat, lng);
+            let map = new google.maps.Map(document.getElementById('map'), {
+              center:latLng,
+              zoom: 18,
+              mapTypeId:'roadmap',
+            });
+            map.setCenter(resultado[0].geometry.location)
+            const infoWindow = new google.maps.InfoWindow({
+              content: "Si no es esta tu ubicacion, da click en la ubicacion correcta.",
+            });
+             var marker = new google.maps.Marker({                
+              position: latLng, 
+              map,
+              title:'Ubicacion actual'
+            });
+            infoWindow.open({
+              anchor: marker,
+              map,
+              shouldFocus: false,
+            });   
+            map.addListener("click", (mapsMouseEvent) => {
+              // Close the current InfoWindow.
+              infoWindow.close();
+              console.log(mapsMouseEvent)
+              map.setCenter(mapsMouseEvent.latLng)
+              marker.setMap(null)
+              this.coordenadas = {
+                lat : mapsMouseEvent.latLng.lat(),
+                lng : mapsMouseEvent.latLng.lng()
+              }
+              marker = new google.maps.Marker({                
+                position: mapsMouseEvent.latLng, 
+                map,
+                title:'Ubicacion actual'
+              });
+              infoWindow.open({
+                anchor: marker,
+                map,
+                shouldFocus: false,
+              });  
+            })
+          }
+        });
       }
-      coordenadas.latitude = position.coords.latitude,
-      coordenadas.longitude = position.coords.longitude
-      console.log(coordenadas)
-      const coord  = await this.$axios.$post('/proveedores/guardarCoordenadas',coordenadas)
+    },
+    async guardarCoordenadas(){
+      this.coordenadas._id = this.$auth.user.idEmpresa
+      const coord  = await this.$axios.$post('/proveedores/guardarCoordenadas',this.coordenadas)
       console.log(coord)
       if(coord.success){
         this.$router.go(0)
--- server/routes/proveedoresCrud.js
+++ server/routes/proveedoresCrud.js
@@ -8,9 +8,8 @@ const authe = require('../middlewares/midAuth.js')
 
 router.post('/guardarCoordenadas', authe, async (req,res)=>{
     console.log(req.body)
-    let coordenadass = [req.body.longitude,req.body.latitude]
-    console.log(coordenadass)
-    const coord = await Proveedores.updateOne({_id:req.body._id},{$set:{"coordenadas.coordinates":coordenadass,coordenadasProveedor:{longitude:req.body.longitude,latitude:req.body.latitude}}})
+    let coordenadass = [req.body.lng,req.body.lat]
+    const coord = await Proveedores.updateOne({_id:req.body._id},{$set:{"coordenadas.coordinates":coordenadass,coordenadasProveedor:{longitude:req.body.lng,latitude:req.body.lat}}})
     if(coord.modifiedCount>0){
         res.json({
             success:true,
